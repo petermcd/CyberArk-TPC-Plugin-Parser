261c37d7044ef1097802d5fa84f91d541fc6a3fc
Peter McDonald <me@petermcdonald.co.uk>
2026-07-14 10:37:36 +0100

    Claude assisted minor refactor

--- tpc_plugin_parser/lexer/utilities/regex.py
+++ tpc_plugin_parser/lexer/utilities/regex.py
@@ -1,17 +1,12 @@
 """List of regex the lexer uses."""
 
-ASSIGNMENT: str = r"^(?:[\s]*)(?P<name>[\w]+)(?:(?:[\s]*)(?P<equals>=)(?:(?:[\s]*)(?P<value>[^\r\n]*))?)?(?:[\s]*)$"
-COMMENT: str = r"^(?:[\s]*)(?P<comment>(?:[#;]+)(?:.*))(?:[\s]*)$"
+ASSIGNMENT: str = r"^\s*(?P<name>\w+)(?:\s*(?P<equals>=)(?:\s*(?P<value>[^\r\n]*))?)?\s*$"
+COMMENT: str = r"^\s*(?P<comment>[#;]+.*)\s*$"
 CPM_PARAMETER_VALIDATION: str = (
-    r"^(?:[\s]*)(?P<name>[\w\\]+)(?:(?:[\s]*,[\s]*)(?:source)(?:[\s]*)=(?:[\s]*)"
-    r"(?P<source>[^, ]*))(?:(?:[\s]*,[\s]*)(?:mandatory)(?:[\s]*)=(?:[\s]*)"
-    r"(?P<mandatory>[^,]*))?(?:(?:[\s]*,[\s]*)(?:allowcharacters)(?:[\s]*)=(?:[\s]*)(?P<allowcharacters>.*))?$"
+    r"^\s*(?P<name>[\w\\]+)(?:\s*,\s*source\s*=\s*(?P<source>[^, ]*))"
+    r"(?:\s*,\s*mandatory\s*=\s*(?P<mandatory>[^,]*))?"
+    r"(?:\s*,\s*allowcharacters\s*=\s*(?P<allowcharacters>.*))?$"
 )
-FAIL_STATE: str = (
-    r"^(?:[\s]*)(?P<name>[\w]+)(?:[\s]*)=(?:[\s]*)(?:fail)(?:[\s]*)\((?:[\s]*)"
-    r"(?P<message>.*),(?:[\s]*)(?P<code>[0-9]+)\)(?:[\s]*)$"
-)
-SECTION_HEADER: str = r"^(?:[\s]*)\[(?P<name>[\w]+(?:[\s]+[\w]+)*)](?:[\s]*)$"
-TRANSITION: str = (
-    r"^(?:[\s]*)(?P<current>[\w]+)(?:[\s]*,[\s]*)(?P<condition>[\w]+)(?:[\s]*,[\s]*)(?P<next>[\w]+)(?:[\s]*)$"
-)
+FAIL_STATE: str = r"^\s*(?P<name>\w+)\s*=\s*fail\s*\(\s*(?P<message>.*),\s*(?P<code>[0-9]+)\)\s*$"
+SECTION_HEADER: str = r"^\s*\[(?P<name>\w+(?:\s+\w+)*)]\s*$"
+TRANSITION: str = r"^\s*(?P<current>\w+)\s*,\s*(?P<condition>\w+)\s*,\s*(?P<next>\w+)\s*$"
